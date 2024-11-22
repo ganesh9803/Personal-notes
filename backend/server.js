b67dbd198d7f3e0c2d448e9635343c789942c645
ganesh9803 <santhosh.ganesh1023@gmail.com>
2024-11-23 02:57:39 +0530

    v

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,6 @@ import notesRoutes from './routes/index.js';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 5000 
 
 // Database connection
 connectDB();
@@ -19,11 +18,10 @@ app.use(express.json());
 // Routes
 app.use('/api/notes', notesRoutes);
 
-// Start server
-app.get('/',(req,res)=> {
-  res.send("API Working")
-})
+// Default Route
+app.get('/', (req, res) => {
+  res.send("API Working");
+});
 
-app.listen(port, ()=>console.log('Sever started on PORT : ' + port))
-
+// Export the app for Vercel
 export default app;
